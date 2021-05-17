3e6e3928663edf5c4ed3abe511d9d7b901c58f53
Gabriel Pérez <gabriel@garox.org>
2021-05-17 14:50:24 -0400

    change buy

--- src/components/eth/eth.tsx
+++ src/components/eth/eth.tsx
@@ -2,15 +2,13 @@ import React, { useEffect, useState } from "react"
 import Web3 from "web3"
 import { Contract } from "web3-eth-contract"
 import { toast } from "react-toastify"
-import { useForm } from "react-hook-form"
 import styled from "styled-components"
 import tw from "twin.macro"
-
-import useConstant from "use-constant"
 
 import abi from "../../assets/abi.json"
 import CountInput from "../count-input"
 import { CONTRACT_ADDRESS } from "../../helpers/constants"
+import { Button, PageParagraph, PageSubtitle } from "../common"
 
 const getWeb3 = async () => {
   if (typeof window != undefined && window["ethereum"]) {
@@ -21,41 +19,32 @@ const getWeb3 = async () => {
   return new Web3(Web3.givenProvider || "http://127.0.0.1:7545")
 }
 
+const estimateGas = (number: number) =>
+  Math.floor((30763 * number + 35236) * 1.5)
+
 const mint = async (contract: Contract, number: number) => {
-  await contract.methods.mintEMU(number).send({
-    from: contract.defaultAccount,
-    value: 50000000000000000 * number,
-    gas: 3_800_000,
-  })
+  try {
+    await contract.methods.mintEMU(number).send({
+      from: contract.defaultAccount,
+      value: 60000000000000000 * number,
+      gas: estimateGas(number),
+    })
+    toast.success(`${number} EMUS Minted`)
+  } catch (error) {
+    toast.error(`Failed to mint. Error: ${error.message}`)
+  }
 }
 
-type Inputs = {
-  contract: string
-  address: string
-}
-
-const TextInput = styled.input`
-  ${tw`text-sm sm:text-base relative w-full border rounded placeholder-gray-400 focus:border-indigo-400 focus:outline-none py-2  pl-12`};
+const SubmitButton = styled(Button)`
+  ${tw`w-full`};
 `
 
-const Label = styled.label`
-  ${tw`mb-1 text-xs sm:text-sm tracking-wide text-gray-600`};
+const AccentParagraph = styled(PageParagraph)`
+  color: ${({ theme }) => theme.colors.accent};
 `
-
-const SubmitButton = styled.button`
-  ${tw`inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white`};
-  ${({ theme }) => theme.backgrounds.accent};
-  ${({ theme }) => theme.backgrounds.hoverAccent};
-  ${({ theme }) => theme.backgrounds.focusAccent};
-`
-
 const Container = styled.div`
   ${tw`mt-5 md:col-span-2 w-3/5 mx-auto p-4 mb-5 shadow-md rounded`};
 `
-
-const connect = async (data: Inputs) => {
-  console.log(data)
-}
 
 const valueInBounds = (n: number) => Math.min(Math.max(n, 0), 250)
 
@@ -69,22 +58,20 @@ const createSignTypedData = (web3: Web3) => (object: unknown) =>
 
 const Eth = React.forwardRef<HTMLDivElement>((props, ref) => {
   const [contract, setContract] = useState<Contract | undefined>()
-  const { register, handleSubmit, watch } = useForm<Inputs>()
   const [numberOfTokens, setNumberOfTokens] = useState(0)
 
-  const connect = async (data: Inputs) => {
-    console.log(data)
+  const connect = async () => {
     const web3 = await getWeb3()
     const contract = new web3.eth.Contract(abi as any, CONTRACT_ADDRESS)
-    contract.defaultAccount = data.address
-
+    const [address] = await web3.eth.getAccounts()
+    contract.defaultAccount = address
     setContract(contract)
+    toast.success("Wallet successfully connected")
   }
   useEffect(() => {
     const init = async () => {
       const web3 = await getWeb3()
-      const accounts = await web3.eth.getAccounts()
-      const address = accounts[0]
+      const [address] = await web3.eth.getAccounts()
 
       if (!address) return
       const signTypedData = createSignTypedData(web3)
@@ -106,40 +93,45 @@ const Eth = React.forwardRef<HTMLDivElement>((props, ref) => {
 
       console.log(result)
     }
-    init()
+    // init()
   }, [])
   return (
     <>
       <Container ref={ref}>
-        <form onSubmit={handleSubmit(connect)}>
-          <div>
-            <Label htmlFor="address">Your address</Label>
-            {/* include validation with required or other standard HTML validation rules */}
-            <TextInput name="address" id="address" {...register("address")} />
-          </div>
+        <PageSubtitle>Buy now!</PageSubtitle>
 
-          <SubmitButton type="submit"> submit</SubmitButton>
-        </form>
+        <div>
+          {contract ? (
+            <>
+              <AccentParagraph>
+                Wallet: {contract.defaultAccount}
+              </AccentParagraph>
+              <CountInput
+                value={numberOfTokens}
+                setNumberOfTokens={setNumberOfTokens}
+                onDecrement={() =>
+                  setNumberOfTokens(value => valueInBounds(value - 1))
+                }
+                onIncrement={() =>
+                  setNumberOfTokens(value => valueInBounds(value + 1))
+                }
+              />
+              <br />
+              <SubmitButton onClick={() => mint(contract, numberOfTokens)}>
+                MINT
+              </SubmitButton>
+            </>
+          ) : (
+            <>
+              <PageParagraph>
+                In order to buy, first connect your wallet with the button
+                below!
+              </PageParagraph>
+              <SubmitButton onClick={connect}> Connect wallet</SubmitButton>
+            </>
+          )}
+        </div>
       </Container>
-      <div>
-        {contract && (
-          <>
-            <CountInput
-              value={numberOfTokens}
-              setNumberOfTokens={setNumberOfTokens}
-              onDecrement={() =>
-                setNumberOfTokens(value => valueInBounds(value - 1))
-              }
-              onIncrement={() =>
-                setNumberOfTokens(value => valueInBounds(value + 1))
-              }
-            />
-            <SubmitButton onClick={() => mint(contract, numberOfTokens)}>
-              MINT
-            </SubmitButton>
-          </>
-        )}
-      </div>
     </>
   )
 })
