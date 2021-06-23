e3fd2d361e9b48f9582f599e7cbe8775c1b18591
Gabriel Pérez <gabriel@garox.org>
2021-06-23 17:18:13 -0400

    use cid

--- src/components/nft-list.tsx
+++ src/components/nft-list.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useMemo, useState } from "react"
+import React, { FC, useEffect, useState } from "react"
 import type IPFS from "ipfs-core/src/components"
 
 import type { Contract } from "web3-eth-contract"
@@ -7,16 +7,15 @@ import tw from "twin.macro"
 
 import { normalize } from "eth-sig-util"
 
-import { waitForNode } from "../helpers/waitForNode"
-
-import "react-image-crop/dist/ReactCrop.css"
-import { useQuery } from "react-query"
 import { ManageNFT } from "./manage-nft"
 import { axiosInstance } from "../helpers/axios"
 
-const Spacer = styled.div`
-  padding-bottom: 100px;
+import "react-image-crop/dist/ReactCrop.css"
+
+const Container = styled.div`
+  ${tw`grid gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mx-auto justify-center p-4`};
 `
+const FALLBACK_CID = "QmUwtG6Q1zPLWd4rfk5RyQCE35wJK3CVXXnDWHyiKbNUvA"
 
 type NFTListProps = {
   contract: Contract
@@ -29,14 +28,12 @@ const loadNFTs = async (contract: Contract) => {
   const totalSupply = await contract.methods.totalSupply().call()
   const account = contract.defaultAccount
   const balanceOf = Number(await contract.methods.balanceOf(account).call())
-  console.log(totalSupply, account, balanceOf)
   //   if (balanceOf == 0) return []
 
   const NFTs: number[] = []
   for (let i = 0; i < totalSupply; i++) {
     const id = i + 1
     const owner = await contract.methods.ownerOf(id).call()
-    console.log("a", owner)
 
     if (normalize(owner) === normalize(account)) NFTs.push(id)
 
@@ -46,16 +43,16 @@ const loadNFTs = async (contract: Contract) => {
 }
 
 const NFTList: FC<NFTListProps> = ({ contract, node, setOpen, hash }) => {
-  const [data, setData] = useState([])
+  const [ids, setIds] = useState<(number | string)[]>(null)
   const [isLoading, setIsLoading] = useState(true)
   const [error, setError] = useState(false)
-  const [urls, setUrls] = useState<string[]>([])
+  const [urls, setUrls] = useState<{ id: string | number; cid: string }[]>(null)
 
   // load owned nfts
   useEffect(() => {
     loadNFTs(contract)
       .then(val => {
-        setData(val)
+        setIds(val)
         setIsLoading(false)
       })
       .catch(setError)
@@ -63,29 +60,40 @@ const NFTList: FC<NFTListProps> = ({ contract, node, setOpen, hash }) => {
 
   // load ipfs CID of owned nfts
   useEffect(() => {
-    if (data) {
+    if (ids) {
       axiosInstance
-        .post<{ path: string; id: string }[]>("/nft/getNFTs", { ids: data })
-        .then(response => setUrls(response.data.map(({ path, id }) => id)))
+        .post<{ path: string; id: string }[]>("/nft/getNFTs", {
+          ids,
+        })
+        .then(response => {
+          const resolved = ids.map(nft => {
+            const cid =
+              (response.data ?? []).find(({ id }) => id == nft)?.path ??
+              FALLBACK_CID
+            console.log(cid)
+            return { id: nft, cid }
+          })
+
+          setUrls(resolved)
+        })
     }
-  }, [data])
+  }, [ids])
 
   if (isLoading) return <p>loading...</p>
   if (error) return <p>error...</p>
-  console.log(data, urls)
 
   return (
-    <>
-      {data.map(id => (
+    <Container>
+      {(urls ?? []).map(({ id, cid }) => (
         <ManageNFT
           key={`nft-${id}`}
           node={node}
-          id={id}
-          hash={hash}
+          id={Number(id)}
+          hash={cid}
           openModal={() => setOpen(true)}
         />
       ))}
-    </>
+    </Container>
   )
 }
 
