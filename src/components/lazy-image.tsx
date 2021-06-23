8243f5adffdeec06c9c63f34a362282794c2b8b9
Gabriel Pérez <gabriel@garox.org>
2021-06-23 09:49:35 -0400

    improve lazy-image

--- src/components/lazy-image.tsx
+++ src/components/lazy-image.tsx
@@ -1,13 +1,38 @@
-import React, { FC } from "react"
+import React, { FC, useState } from "react"
+import Skeleton from "react-loading-skeleton"
 
 type ImageUploadModalProps = {
   source: string
-}
+} & React.DetailedHTMLProps<
+  React.ImgHTMLAttributes<HTMLImageElement>,
+  HTMLImageElement
+>
 
-export const LazyImage: FC<ImageUploadModalProps> = ({ source }) => {
+const FALLBACK_SOURCE =
+  "https://github.com/make-github-pseudonymous-again/pixels/raw/main/1x1%23000000.png"
+
+export const LazyImage: FC<ImageUploadModalProps> = ({ source, ...props }) => {
+  const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(true)
+
+  const onError = () => {
+    setLoading(false)
+    setError(true)
+  }
+
+  const onLoad = () => {
+    setLoading(false)
+  }
   return (
     <div>
-      <img src={source} />
+      {loading && <Skeleton height={props.height} width={props.width} />}
+      <img
+        {...props}
+        style={loading ? { display: "none" } : props.style ?? {}}
+        src={!error ? source : FALLBACK_SOURCE}
+        onError={onError}
+        onLoad={onLoad}
+      />
     </div>
   )
 }
