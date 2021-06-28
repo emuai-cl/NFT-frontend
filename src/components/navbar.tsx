76c1808296ea3a6e925b4c4c0cb3bd085637c7a5
Belen Cespedes <belencatalina.cespedes@gmail.com>
2021-06-28 18:21:33 -0400

    navbar-mobile

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,21 +4,21 @@ import styled, { css } from "styled-components"
 import { Link } from "gatsby"
 
 const Navbar = styled.nav`
-  ${tw`w-full z-30 top-0  text-white py-1 lg:py-4`};
+  ${tw`w-full z-30 top-0 lg:bg-transparent bg-gray-100 text-white py-1 lg:py-4`};
   position: absolute;
   top: 0;
 `
 
 const NavbarContainer = styled.div`
-  ${tw`w-full container  mx-auto flex flex-wrap items-center justify-between mt-0 px-2 py-2 lg:py-6`};
+  ${tw`w-full container   mx-auto flex flex-wrap items-center justify-between mt-0 px-2 py-2 lg:py-6`};
 `
 
 const InnerContainer = styled.div`
-  ${tw`pl-4 flex items-center`};
+  ${tw`pl-4  flex items-center`};
 `
 
 const Content = styled.div<{ open: boolean }>`
-  ${tw`w-full flex-grow lg:flex lg:items-center  lg:w-auto lg:block mt-2 lg:mt-0 text-black p-4 lg:p-0 z-20`};
+  ${tw`w-full  flex-grow lg:flex lg:items-center  lg:w-auto lg:block mt-2 lg:mt-0 text-black p-4 lg:p-0 z-20`};
   ${({ open }) =>
     !open &&
     css`
@@ -30,18 +30,16 @@ const List = styled.ul`
   ${tw`lg:flex flex-1 w-auto justify-end  items-center`};
 `
 const ListItem = styled.li`
-  ${tw`mr-3 `};
+  ${tw`mr-3 text-center  `};
 `
-const MobileListItem = styled.li`
-  ${tw`mr-12`};
-`
+
 const StyledLink = styled(Link)`
-  ${tw`inline-block py-2 px-4 text-white  font-bold no-underline`};
-  text-shadow: 1px 1px rgba(0, 0, 0, 0.5);
+  ${tw`inline-block py-2 px-4 lg:text-white text-gray-500  font-bold no-underline`};
+  lg:text-shadow: 1px 1px rgba(0, 0, 0, 0.5);
 `
 
 const SpecialLink = styled(Link)`
-  ${tw`mx-auto lg:mx-0 text-white font-extrabold rounded mt-4 lg:mt-0 mx-5 lg:mx-5 py-4 px-8 shadow-md`};
+  ${tw`mx-auto lg:mx-0 text-white lg:font-extrabold rounded mt-4 lg:mt-0 mx-5 lg:mx-5 lg:py-4 py-2 lg:px-8 px-4 shadow-md`};
   ${({ theme }) => theme.backgrounds.accent};
   ${({ theme }) => theme.backgrounds.hoverAccent};
   ${({ theme }) => theme.backgrounds.focusAccent};
@@ -52,7 +50,7 @@ const MobileMenuContainer = styled.div`
 `
 
 const MobileMenuButton = styled.button`
-  ${tw`flex items-center px-3 py-2 border rounded text-gray-300 border-gray-300 hover:text-white hover:border-white appearance-none focus:outline-none`};
+  ${tw`flex items-center px-3 py-2 border rounded text-gray-500 border-gray-500 hover:text-white hover:border-white appearance-none focus:outline-none`};
 `
 
 const Svg = styled.svg`
