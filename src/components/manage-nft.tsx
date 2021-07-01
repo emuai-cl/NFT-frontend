1eeb1808383d03b0cfb3290b2e087c842d0cd37c
Gabriel Pérez <gabriel@garox.org>
2021-07-01 15:00:19 -0400

    use image

--- src/components/manage-nft.tsx
+++ src/components/manage-nft.tsx
@@ -1,25 +1,19 @@
-import React, { useEffect, useState } from "react"
+import React from "react"
 import type IPFS from "ipfs-core/src/components"
 
 import styled from "styled-components"
 import tw from "twin.macro"
 
-import { Button, PageLink } from "./common"
-import Navbar from "./navbar"
+import { Button } from "./common"
 
-import { ImageUpload } from "./image-upload"
-import { CustomModal } from "./modal"
-
-import { useContract } from "../hooks/useContract"
-import { useConnect } from "../hooks/useConnect"
 import { useWeb3 } from "../hooks/useWeb3"
 import { useSignNFT } from "../hooks/useSignNFT"
 
 import "react-image-crop/dist/ReactCrop.css"
 import { LazyImage } from "./lazy-image"
 
-const Spacer = styled.div`
-  padding-bottom: 100px;
+const NFTCard = styled.div`
+  ${tw`shadow-md p-4 bg-white rounded`};
 `
 
 type ManageNFTProps = {
@@ -43,26 +37,15 @@ export const ManageNFT: React.FC<ManageNFTProps> = ({
   }
 
   return (
-    <>
-      <Navbar />
-      <Spacer />
-
+    <NFTCard>
+      <LazyImage
+        height={150}
+        width={150}
+        source={`https://ipfs.infura.io:5001/api/v0/cat?arg=${hash}`}
+      />
       <Button onClick={openModal}>open modal</Button>
-      {hash && (
-        <>
-          <p>{hash}</p>
-          <LazyImage
-            source={`https://ipfs.infura.io:5001/api/v0/cat?arg=${hash}`}
-          />
-          <PageLink
-            href={`https://ipfs.infura.io:5001/api/v0/cat?arg=${hash}`}
-            target="_blank"
-          >
-            see it
-          </PageLink>
-        </>
-      )}
+
       <Button onClick={onClick}>Confirm</Button>
-    </>
+    </NFTCard>
   )
 }
