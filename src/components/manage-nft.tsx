0d9d62d3a2285bbe2be70efaa39a1ee350b45dd2
Gabriel Pérez <gabriel@garox.org>
2021-07-05 00:50:11 -0400

    create functional manage nft

--- src/components/manage-nft.tsx
+++ src/components/manage-nft.tsx
@@ -4,48 +4,106 @@ import type IPFS from "ipfs-core/src/components"
 import styled from "styled-components"
 import tw from "twin.macro"
 
-import { Button } from "./common"
+import {
+  CancelButton,
+  ConfirmButton,
+  FullButton,
+  GroupContainer,
+} from "./common"
 
 import { useWeb3 } from "../hooks/useWeb3"
 import { useSignNFT } from "../hooks/useSignNFT"
 
 import "react-image-crop/dist/ReactCrop.css"
 import { LazyImage } from "./lazy-image"
+import { useHash } from "../hooks/useHash"
+import { useSetHash } from "../hooks/useSetHash"
+import { useEditing } from "../hooks/useEditing"
+import { useSetEditing } from "../hooks/useSetEditing"
+import { useCallback } from "react"
+import { axiosInstance } from "../helpers/axios"
+import { MdEdit } from "react-icons/md"
+import { AiOutlineCheck, AiOutlineClose } from "react-icons/ai"
 
 const NFTCard = styled.div`
   ${tw`shadow-md p-4 bg-white rounded`};
+`
+
+const StyledImage = styled(LazyImage)`
+  ${tw`mx-auto my-4`};
+`
+const EditIcon = styled(MdEdit)`
+  ${tw`fill-current w-4 h-4 mr-2`};
+`
+const ConfirmIcon = styled(AiOutlineCheck)`
+  ${tw`fill-current w-4 h-4 mr-2`};
+`
+
+const CancelIcon = styled(AiOutlineClose)`
+  ${tw`fill-current w-4 h-4 mr-2`};
 `
 
 type ManageNFTProps = {
   node: IPFS
   id: number
   openModal: () => void
-  hash: string
+  cid: string
 }
 
-export const ManageNFT: React.FC<ManageNFTProps> = ({
-  node,
-  id,
-  openModal,
-  hash,
-}) => {
+export const ManageNFT: React.FC<ManageNFTProps> = ({ id, openModal, cid }) => {
   const web3 = useWeb3()
   const signNFT = useSignNFT(web3)
 
-  const onClick = async () => {
-    const signature = await signNFT(hash, id)
-  }
+  const hash = useHash()
+  const editingID = useEditing()
+  const setEditing = useSetEditing()
+  const setHash = useSetHash()
 
+  const onClick = useCallback(async () => {
+    const signature = await signNFT(hash, id)
+    const { data } = await axiosInstance.post("/nft/updateNFT", {
+      path: hash,
+      id,
+      signature,
+    })
+
+    console.log(data)
+  }, [hash, id])
+
+  const handleEdit = useCallback(() => {
+    setEditing(id)
+    openModal()
+  }, [id])
+
+  const handleCancel = useCallback(() => {
+    setEditing(null)
+    setHash(null)
+  }, [])
   return (
     <NFTCard>
-      <LazyImage
+      <StyledImage
         height={150}
         width={150}
-        source={`https://ipfs.infura.io:5001/api/v0/cat?arg=${hash}`}
+        source={`https://ipfs.infura.io:5001/api/v0/cat?arg=${
+          editingID === id && hash ? hash : cid
+        }`}
       />
-      <Button onClick={openModal}>open modal</Button>
-
-      <Button onClick={onClick}>Confirm</Button>
+      {editingID === id ? (
+        <GroupContainer>
+          <CancelButton onClick={handleCancel}>
+            <CancelIcon /> <span>Cancel</span>
+          </CancelButton>
+          <ConfirmButton onClick={onClick}>
+            <ConfirmIcon />
+            <span>Confirm</span>
+          </ConfirmButton>
+        </GroupContainer>
+      ) : (
+        <FullButton onClick={handleEdit}>
+          <EditIcon />
+          <span>Edit</span>
+        </FullButton>
+      )}
     </NFTCard>
   )
 }
