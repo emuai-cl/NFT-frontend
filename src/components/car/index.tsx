67bb46aecc2b96d4f4e3742a903b40969fcbb125
Gabriel Pérez <gabriel@garox.org>
2021-07-14 22:54:36 -0400

    refactor car

---
+++ src/components/car/index.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { SubtitleWithLine } from "../subtitle-with-line"
+import { LoadCar } from "./load-car"
+
+export default () => (
+  <>
+    <SubtitleWithLine color="#444">3D Model</SubtitleWithLine>
+    <LoadCar />
+  </>
+)
