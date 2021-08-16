fdfca1bc12d8d8710a3dcd34084f1062761ea3a2
Machuzka <41125296+Machuzka@users.noreply.github.com>
2021-08-16 18:42:49 -0400

    Cambio de texto

--- src/components/powered.tsx
+++ src/components/powered.tsx
@@ -20,16 +20,18 @@ export const Powered = () => {
           Powered by <Accent>Ethereum</Accent>
         </PageSubtitle>
         <PageParagraph>
-          This an{" "}
+          We are a Solar Racecar Team that wants to change how funding is done.
+          We give you the opportunity to buy space on our solar car's surface where
+          you can put an image of your choice. Each 1.5 x 1.5 cm square will be an {" "}
           <PageLink
             href="https://eips.ethereum.org/EIPS/eip-721"
             target="_blank"
           >
             ERC-721
           </PageLink>{" "}
-          Non Fungible Token (NFT) that will directly fund the our solar car.
+          Non Fungible Token (NFT) that will directly fund our solar car.
           Moreover, each NFT owner will have the ability to add any 100x100
-          picture he likes directly on our car!.
+          picture he likes directly on our car!
         </PageParagraph>
       </div>
       <div>
