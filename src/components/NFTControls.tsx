c1c4bbfee35f939912eeb9ceeace0d9bb08bb10e
Gabriel Pérez <gabriel@garox.org>
2021-11-01 13:07:15 -0300

    use PageLink styles

--- src/components/NFTControls.tsx
+++ src/components/NFTControls.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 import styled from "styled-components"
 import tw from "twin.macro"
+import { PageLink } from "./common"
 
 export const Container = styled.div`
   ${tw`mx-auto`};
@@ -10,8 +11,16 @@ export const Container = styled.div`
 export const Controls = ({ currentId }: { currentId: number }) => {
   return (
     <Container>
-      {currentId > 1 && <Link to={`/nft?id=${currentId - 1}`}>previous</Link>}
-      {currentId < 5940 && <Link to={`/nft?id=${currentId + 1}`}>next</Link>}
+      {currentId > 1 && (
+        <PageLink as={Link} to={`/nft?id=${currentId - 1}`}>
+          previous
+        </PageLink>
+      )}
+      {currentId < 5940 && (
+        <PageLink as={Link} to={`/nft?id=${currentId + 1}`}>
+          next
+        </PageLink>
+      )}
     </Container>
   )
 }
